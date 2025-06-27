7518a669ee19da45566a4defbf189ffad288e45a
Rangga Manggala Yudha <ranggaamyd@gmail.com>
2025-06-27 10:50:44 +0700

    Updated CandidateDetail.tsx

--- src/pages/Recruitments/CandidateDetail.tsx
+++ src/pages/Recruitments/CandidateDetail.tsx
@@ -95,7 +95,7 @@ export const CandidateDetail = ({ candidate, yoe, onClose }: { candidate: Candid
     };
 
     return (
-        <div className="p-6 space-y-6 bg-gray-50 dark:bg-gray-900 min-h-screen">
+        <div className="p-6 space-y-6 min-h-screen">
             {lightboxOpen && (
                 <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50" onClick={closeLightbox}>
                     <div className="relative max-w-4xl max-h-[90vh] p-4">
